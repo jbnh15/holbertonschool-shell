2fb5a5f740a47f076e6e11735736b8b52064e246
jbnh <jose.noguera@anahuac.mx>
2026-04-15 12:13:12 -0600

    Standardize script names and fix 0-current_working_directory newline

---
+++ cleanup.py
@@ -0,0 +1,14 @@
+import os
+def fix_file(path, content):
+    with open(path, "wb") as f:
+        f.write(content.encode("utf-8").replace(b"\r\n", b"\n"))
+
+# Fix task 0 in basics
+fix_file("basics/0-current_working_directory", "#!/bin/bash\npwd\n")
+
+# Cleanup init_files
+if os.path.exists("init_files_variables_and_expansions/1-hello_betty") and os.path.exists("init_files_variables_and_expansions/1-hello_you"):
+    os.remove("init_files_variables_and_expansions/1-hello_betty")
+
+if os.path.exists("init_files_variables_and_expansions/10-love_exponent_ans") and os.path.exists("init_files_variables_and_expansions/10-love_exponent_breath"):
+    os.remove("init_files_variables_and_expansions/10-love_exponent_ans")
